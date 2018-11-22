7a9212e4ead4bf19cbec91b30d5bb55713f404e9
pravallikasykam <42571340+pravallikasykam@users.noreply.github.com>
2018-11-22 10:57:34 +0530

    Create minimum.py

---
+++ minimum.py
@@ -0,0 +1,11 @@
+min = int(raw_input())
+def smallest(arr,n) :
+	min = arr[0]
+	for i in range(1,n) :
+		if arr[i] < min :
+			min = arr[i]
+	return min
+arr = [1,2,3]
+n = len(arr)
+ans = smallest(arr,n)
+print(ans)
